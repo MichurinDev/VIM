d9659d6581593b888904ac9d93acfecf3ea5ba67
MichurinDev <420520052006MfDeveloper>
2023-01-06 18:16:43 +0700

    Добавлена новая квартира (20)

--- get_source_flat_images.py
+++ get_source_flat_images.py
@@ -2,7 +2,7 @@ import os
 
 pre_source = "https://github.com/MichurinDev/VIM/tree/master/flats"
 
-with open("flat_image_source.txt", "w") as f:
+with open("flat_image_source.txt", "w", encoding="utf-8") as f:
     for dir in os.listdir("flats"):
         image_srcs = list(map(lambda x: f'<img src="{pre_source}/{dir}/{x}">', list(filter(lambda x: "pdf" not in x, [file for file in os.listdir(f"flats/{dir}")]))))
         f.write(" ".join(image_srcs) + "\n")
