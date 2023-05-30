edfa3f580ead9cccdb1ffc79f96b1e093d5c68cc
Michurin Andrey <andreymithurin00@gmail.com>
2023-05-31 00:33:21 +0700

    Незначительные изменения + новый файл с тэгами

--- ChangeImage.py
+++ ChangeImage.py
@@ -38,30 +38,28 @@ def change_img_size(path):
     return "Изображение сжато"
 
 
-def get_img_tag():
+def get_img_tag(origin):
     pre_source = \
         "https://raw.githubusercontent.com/MichurinDev/VIM/master/flats"
 
     with open("flat_image_source.txt", "w", encoding="utf-8") as f:
-        for dir in os.listdir("flats"):
+        for dir in os.listdir(origin):
             image_srcs = list(
                 map(lambda x: f'<img src="{pre_source}/{dir}/{x}">',
                     list(filter(
-                        lambda x: "pdf" not in x,
-                        [file for file in os.listdir(f"flats/{dir}")]))))
+                        lambda x: "txt" not in x,
+                        [file for file in os.listdir(f"{origin}/{dir}")]))))
 
             f.write(" ".join(image_srcs) + "\n")
 
     return "Тэги получены"
 
 
-def MakeDirectory(origin, count=10):
-    with open('./flats.csv', encoding="utf8") as csvfile:
+def MakeDirectory(origin):
+    with open('./flats2.csv', encoding="utf8") as csvfile:
         reader = csv.reader(csvfile, delimiter=';', quotechar='"')
         for index, row in enumerate(reader):
-            if index > count:
-                break
-            
+
             if index != 0:
                 path = f"{origin}/{' '.join(row[1:9]).lower()}_{''.join([random.choice('abcdifghijklmnopqrstuvqxwz1234567890') for _ in range(10)])}"
                 os.makedirs(path)
@@ -70,20 +68,21 @@ def MakeDirectory(origin, count=10):
                     f.write(row[-1])
                     f.close()
 
-                print(f"{index}/{count}")
     print("Директории созданы")
 
 
-origin = "flats"
+origin = "new_flats"
+exceptions = ['комфорт двухкомнатная 50-100 современный светлая холодное яркая семья ( пара+1)_alwgmczmiz']
 
-# for i, dir in enumerate(os.listdir(origin)):
-#     for j, file in enumerate(os.listdir(f"{origin}/{dir}")):
-#         path = f"{origin}/{dir}/{file}"
-#         if path.split('.')[-1].lower() in ["png", "jpg", "jpeg"]:
-#             print(f'{i + 1}/{len(os.listdir(origin))} | \
-#                   {j + 1}/{len(os.listdir(f"{origin}/{dir}"))}')
-#             print(change_img_size(path))
-#             print(watermark(path))
-#             print()
+for i, dir in enumerate(os.listdir(origin)):
+    for j, file in enumerate(os.listdir(f"{origin}/{dir}")):
+        path = f"{origin}/{dir}/{file}"
+        if path.split('.')[-1].lower() in ["png", "jpg", "jpeg"]:
+            print(f'{i + 1}/{len(os.listdir(origin))} | \
+                  {j + 1}/{len(os.listdir(f"{origin}/{dir}"))}')
+            print(change_img_size(path))
+            if dir not in exceptions:
+                print(watermark(path))
+            print()
 
-print(get_img_tag())
+print(get_img_tag(origin))
